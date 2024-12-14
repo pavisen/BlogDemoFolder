e0d51a5734ed298c27193ac098dcbecd936ebccb
pavisen <pavithrasenthurai@gmail.com>
2024-12-14 13:25:15 -0500

    first commit

---
+++ DSAlgo/src/main/java/numpyNinja/App.java
@@ -0,0 +1,13 @@
+package numpyNinja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
